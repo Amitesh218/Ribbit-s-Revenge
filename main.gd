d3061baef5debeb92e81504d8d32d7d8ce5f100d
Amitesh218 <amiteshrawal1@gmail.com>
2025-03-11 22:12:07 +0530

    added new scene

--- main.gd
+++ main.gd
@@ -3,3 +3,6 @@ extends Node2D
 
 func _on_quitbut_pressed() -> void:
 	get_tree().quit()
+
+func _on_playbut_pressed() -> void:
+	get_tree().change_scene_to_file("res://world.tscn") # Replace with function body.
