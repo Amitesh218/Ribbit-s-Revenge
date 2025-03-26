cc2bd936effc6d61c8ff61555e7b4b15f4c68fa3
Amtiesh218 <amiteshrawal1@gmail.com>
2025-03-26 04:30:07 -0400

    alpha/combat

--- main.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-func _on_quitbut_pressed() -> void:
-	get_tree().quit()
-
-func _on_playbut_pressed() -> void:
-	get_tree().change_scene_to_file("res://world.tscn") # Replace with function body.
