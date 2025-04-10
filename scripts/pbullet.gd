50b96bb3f5b878936a35e5e0decbd6dba594b4d9
Amitesh218 <amiteshrawal1@gmail.com>
2025-04-10 14:36:14 +0530

    added bounce to bullets

--- scripts/pbullet.gd
+++ scripts/pbullet.gd
@@ -1,18 +1,27 @@
 extends CharacterBody2D
 
-@export var speed := 500.0
+@export var speed := 300.0
+@export var lifespan := 2.0  # seconds
 var direction: Vector2 = Vector2.RIGHT
+var life_timer := 0.0
 
 func _ready():
 	direction = direction.normalized()
 
 func _physics_process(delta):
-	var collision = move_and_collide(direction * speed * delta)
+	life_timer += delta
+	if life_timer >= lifespan:
+		queue_free()
+
+	var velocity = direction * speed
+	var collision = move_and_collide(velocity * delta)
 	if collision:
 		var collider = collision.get_collider()
 
 		if collider.name.contains("shooter") or collider.name == "suicidechaser":
 			collider.health -= 5
-		queue_free()
-		
-		
+			queue_free()
+			return
+
+		var normal = collision.get_normal()
+		direction = direction.bounce(normal).normalized()
